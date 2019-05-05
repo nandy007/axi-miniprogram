7fa4c3d403ee409eb8bcaeffb2d149a5ab707203
nandy007 <nandy007@qq.com>
2019-05-05 17:22:54 +0800

    完善框架

--- axicomponent/image/axi-image.js
+++ axicomponent/image/axi-image.js
@@ -8,17 +8,22 @@ ComponentWrapper({
   properties: {
     'src': {
       // memo: '图片地址',
-      type: String
+      type: String,
+      observer: function (v) {
+        var rs = v || '';
+        if (rs.indexOf('http://') !== 0 && rs.indexOf('https://') !== 0) {
+          rs = v ? ComponentWrapper.getAbsolute(v) : '';
+        }
+        console.log(rs)
+        this.setData({
+          trueSrc: rs
+        });
+      }
     },
     'mode': {
       // memo: '图片裁剪、缩放的模式',
       type: String
-    },
-    'src': {
-      // memo: '图片地址',
-      type: String
-    },
-
+    }
   },
 
   /**
@@ -26,19 +31,20 @@ ComponentWrapper({
    */
   data: {
     width: '',
-    height: ''
+    height: '',
+    trueSrc: ''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    errorFunc: function(e) {
-        this.triggerEvent('error', arguments);
+    errorFunc: function (e) {
+      this.triggerEvent('error', arguments);
     },
-    loadFunc: function(e) {
+    loadFunc: function (e) {
       var mode = this.data.mode;
-      if (mode ==='autoFit'){
+      if (mode === 'autoFit') {
         var width = e.detail.width, height = e.detail.height;
         this.setData({
           width: width,
--- axicomponent/include.js
+++ axicomponent/include.js
@@ -1,38 +1,41 @@
 
 const util = {
-  get app(){
+  get app() {
     return getApp();
   }
 };
 
-function delay(cb){
+function delay(cb) {
   var delayTime = 50;
-  setTimeout(function(){
+  setTimeout(function () {
     cb();
   }, delayTime);
 }
 
-function createPageCache(ctx, pageParams) {
+function createPageCache(ctx, pageParams, absolutePath) {
   var _names = {}, _all = [], _models = {};
   var _idCache = {};
-  
+
   var util = {
-    getPageParams: function(){
+    getPagePath: function () {
+      return absolutePath;
+    },
+    getPageParams: function () {
       return pageParams;
     },
-    name: function(name, comp, isDel){
+    name: function (name, comp, isDel) {
       _names[name] = _names[name] || [];
-      if (isDel){
+      if (isDel) {
         var index = _names[name].indexOf(comp);
         if (index > -1) _names[name].splice(index, 1);
-      } else if (comp){
-        if (_names[name].indexOf(comp)===-1) _names[name].push(comp);
+      } else if (comp) {
+        if (_names[name].indexOf(comp) === -1) _names[name].push(comp);
       } else {
         return (_names[name] || []).slice(0);
       }
     },
     all: function (comp, isDel) {
-      if (isDel){
+      if (isDel) {
         var index = _all.indexOf(comp);
         if (index > -1) _all.splice(index, 1);
       } else if (comp) {
@@ -41,15 +44,15 @@ function createPageCache(ctx, pageParams) {
         return _all.slice(0);
       }
     },
-    selectById: function(id){
+    selectById: function (id) {
       // 先从页面找
-      var comp = ctx.selectComponent('#'+id);
+      var comp = ctx.selectComponent('#' + id);
       if (comp) return comp;
       // 页面没有则从缓存里找
       if (idCache[id]) return idCache[id];
       // 缓存没有则从所有组件找
       var all = util.all();
-      for(var i=0, len=all.length;i<len;i++){
+      for (var i = 0, len = all.length; i < len; i++) {
         comp = all[i].selectComponent('#' + id);
         if (comp) {
           // 找到存入缓存，并结束查找
@@ -59,10 +62,10 @@ function createPageCache(ctx, pageParams) {
       }
       return comp;
     },
-    selectByName: function(name){
+    selectByName: function (name) {
       return util.name(name);
     },
-    selectBySelector: function(selector, isFirst){
+    selectBySelector: function (selector, isFirst) {
       // 先找页面
       var comps = Array.prototype.slice.call(ctx.selectAllComponents(selector));
       // if (!isDeep) return comps; 
@@ -76,8 +79,8 @@ function createPageCache(ctx, pageParams) {
     },
     getValueByName(name) {
       var comps = this.selectByName(name) || [], rs = '';
-      for(var i=0, len=comps.length;i<len;i++){
-        if (comps[i].data.checked){
+      for (var i = 0, len = comps.length; i < len; i++) {
+        if (comps[i].data.checked) {
           rs = comps[i].data.value;
           break;
         }
@@ -93,43 +96,43 @@ function createPageCache(ctx, pageParams) {
       }
       return rs;
     },
-    setData: function(){
+    setData: function () {
       ctx.setData.apply(ctx, arguments);
     },
-    registerModel: function(modelInfo, cb){
+    registerModel: function (modelInfo, cb) {
       var models = _models[modelInfo.exp] = _models[modelInfo.exp] || [];
-      if (models.indexOf(modelInfo)===-1){
+      if (models.indexOf(modelInfo) === -1) {
         modelInfo.cb = cb;
         models.push(modelInfo);
       }
       return this.getValueFromModel(modelInfo);
     },
-    getModelCtx: function (modelInfo){
+    getModelCtx: function (modelInfo) {
       return modelInfo.source ? util.selectById(modelInfo.source) : ctx;
     },
-    getValueFromModel: function (modelInfo){
+    getValueFromModel: function (modelInfo) {
       var exps = modelInfo.exp.split('.'), e, comp = util.getModelCtx(modelInfo), data = comp.data, cur;
-      while(e = exps.shift()){
-        cur = (cur||data)[e];
+      while (e = exps.shift()) {
+        cur = (cur || data)[e];
       }
       return cur;
     },
-    updateModel: function (modelInfo, val){
+    updateModel: function (modelInfo, val) {
       var comp = util.getModelCtx(modelInfo);
       var obj = {};
       obj[modelInfo.exp] = val;
       comp.setData(obj, true);
     },
-    triggerModelUpdate: function (exp, v, comp){
+    triggerModelUpdate: function (exp, v, comp) {
       comp = comp || ctx;
       var models = _models[exp] = _models[exp] || [];
-      models.forEach((modelInfo)=>{
+      models.forEach((modelInfo) => {
         var target = util.getModelCtx(modelInfo);
         if (target === comp) modelInfo.cb && modelInfo.cb(v);
-        
+
       });
     },
-    handlerObservers: function (ctx, keyArr){
+    handlerObservers: function (ctx, keyArr) {
       if (keyArr.length === 0) return;
       var observers = ctx.observers;
       if (!observers) return;
@@ -151,11 +154,11 @@ function createPageCache(ctx, pageParams) {
   return util;
 }
 
-function addPageLifetimes(opt) {
+function addPageLifetimes(opt, absolutePath) {
   var onLoad = opt.onLoad;
-  opt.onLoad = function(options) {
-    var pageCache = this.pageCache = util.app.globalData.__framework.pageCache = createPageCache(this, options);
-    
+  opt.onLoad = function (options) {
+    var pageCache = this.pageCache = util.app.globalData.__framework.pageCache = createPageCache(this, options, absolutePath);
+
     // 重写page的setData
     var setData = this.setData;
     this.setData = function (data, noTrigger) {
@@ -165,16 +168,16 @@ function addPageLifetimes(opt) {
         if (!noTrigger) pageCache.triggerModelUpdate(exp, data[exp]);
       }
       setData.apply(this, arguments);
-      if (keyArr.length>0) pageCache.handlerObservers(this, keyArr);
+      if (keyArr.length > 0) pageCache.handlerObservers(this, keyArr);
     };
 
     onLoad && onLoad.call(this);
   };
   var events = ['onShow', 'onUnload'];
 
-  events.forEach((name)=>{
+  events.forEach((name) => {
     var func = events[name];
-    opt[name] = function(){
+    opt[name] = function () {
       util.app.globalData.__framework.pageCache = this.pageCache;
       func && func.apply(this, arguments);
     }
@@ -184,7 +187,7 @@ function addPageLifetimes(opt) {
 
 function addCompLifetimes(opt) {
   var detached = opt.detached;
-  opt.detached = function() {
+  opt.detached = function () {
     var pageCache = util.app.globalData.__framework.pageCache;
 
     // 将组件从names缓存中清除
@@ -202,16 +205,16 @@ function addCompLifetimes(opt) {
   };
 
   var created = opt.created;
-  opt.created = function(){
+  opt.created = function () {
     var pageCache = util.app.globalData.__framework.pageCache;
     pageCache.all(this);
-    
+
     // 重写comp的triggerEvent
     var triggerEvent = this.triggerEvent;
     this.triggerEvent = function (name, params) {
       triggerEvent.apply(this, arguments);
       if (['input', 'blur', 'change'].indexOf(name) > -1) {
-        this.__modelInfo && this.__modelInfo.handler && this.__modelInfo.handler(params||{});
+        this.__modelInfo && this.__modelInfo.handler && this.__modelInfo.handler(params || {});
       }
     };
 
@@ -230,9 +233,9 @@ function addCompLifetimes(opt) {
   };
 
   var events = ['created', 'attatched'];
-  events.forEach((name)=>{
+  events.forEach((name) => {
     var func = opt[name];
-    if(func) opt[name] = function(){
+    if (func) opt[name] = function () {
       var args = arguments;
       delay(() => {
         func.apply(this, args);
@@ -241,15 +244,15 @@ function addCompLifetimes(opt) {
   });
 }
 
-function getMethods(opt, isPage){
+function getMethods(opt, isPage) {
   var methods = isPage ? opt : (function () {
     return opt.methods = opt.methods || {};
   })();
-  if (isPage && opt.methods){
-    for (var k in opt.methods){
-      (function(k){
+  if (isPage && opt.methods) {
+    for (var k in opt.methods) {
+      (function (k) {
         var old = opt[k];
-        opt[k] = function(){
+        opt[k] = function () {
           old && old.apply(this, arguments);
           return opt.methods[k].apply(this, arguments);
         };
@@ -259,19 +262,19 @@ function getMethods(opt, isPage){
   return methods;
 }
 
-function addMethods(opt, absolutePath, isPage){
+function addMethods(opt, absolutePath, isPage) {
   var methods = getMethods(opt, isPage);
 
   var methodNames = ['selectById', 'selectByName', 'selectBySelector', 'getValueByName', 'getValuesByName'];
 
-  methodNames.forEach((methodName)=>{
-    methods[methodName] = function(){
+  methodNames.forEach((methodName) => {
+    methods[methodName] = function () {
       var pageCache = util.app.globalData.__framework.pageCache;
       return pageCache[methodName].apply(pageCache, arguments);
     };
   });
 
-  methods.getAttrValue = function(attrName){
+  methods.getAttrValue = function (attrName) {
     return this.data[attrName];
   };
 }
@@ -281,35 +284,35 @@ function addValueOberser(opt) {
   var properties = opt.properties = opt.properties || {};
 
   // 添加name属性监听
-  var nameProperty = properties.name = properties.name || {type: 'String'};
+  var nameProperty = properties.name = properties.name || { type: 'String' };
   var nameObserver = nameProperty.observer;
-  nameProperty.observer = function(v, o){
+  nameProperty.observer = function (v, o) {
     var pageCache = util.app.globalData.__framework.pageCache;
-    if(o){
+    if (o) {
       // 删除原缓存
       pageCache.name(o, this, true);
     }
-    if (v){
+    if (v) {
       // 添加新缓存
       pageCache.name(v, this);
     }
-    
+
     nameObserver && nameObserver.apply(this, arguments);
   };
 
-  
-  var attrs = Object.keys(properties||{});
+
+  var attrs = Object.keys(properties || {});
 
   // 为所有的属性添加延迟触发
   attrs.forEach((attr) => {
-    
+
     var property = properties[attr];
 
     var observer = property.observer;
 
     if (!observer) return;
-  
-    property.observer = function() {
+
+    property.observer = function () {
       var args = arguments;
       // 延迟触发，确保page的onload先进入
       delay(() => {
@@ -321,16 +324,16 @@ function addValueOberser(opt) {
 }
 
 
-function bindModelHandler(opt){
+function bindModelHandler(opt) {
 
   var properties = opt.properties = opt.properties || {};
 
   var models = {
     text: {
-      init: function(){
+      init: function () {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this;
 
-        var modelVal = pageCache.registerModel(comp.__modelInfo, function(n){
+        var modelVal = pageCache.registerModel(comp.__modelInfo, function (n) {
           comp.setData({
             value: n
           });
@@ -340,7 +343,7 @@ function bindModelHandler(opt){
           value: modelVal
         });
       },
-      handler: function (params){
+      handler: function (params) {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this, modelInfo = comp.__modelInfo;
         var modelAttrName = modelInfo.attr;
         // var exp = comp.data[modelAttrName];
@@ -348,7 +351,7 @@ function bindModelHandler(opt){
       }
     },
     checkbox: {
-      init: function(){
+      init: function () {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this;
 
         var modelVal = pageCache.registerModel(comp.__modelInfo, function (n) {
@@ -362,27 +365,27 @@ function bindModelHandler(opt){
           checked: modelVal.indexOf(comp.data.value) > -1
         });
       },
-      handler: function (params){
+      handler: function (params) {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this, modelInfo = comp.__modelInfo;
         var modelAttrName = modelInfo.attr;
         // var exp = comp.data[modelAttrName];
         var modelValue = pageCache.getValueFromModel(modelInfo).slice(0);
-        if(params.checked){
+        if (params.checked) {
           if (modelValue.indexOf(params.value) < 0) modelValue.push(params.value);
-        }else{
+        } else {
           var index = modelValue.indexOf(params.value);
-          if (index>-1) modelValue.splice(index, 1);
+          if (index > -1) modelValue.splice(index, 1);
         }
         pageCache.updateModel(modelInfo, modelValue);
       }
     },
     radio: {
-      init: function(){
+      init: function () {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this;
 
         var modelVal = pageCache.registerModel(comp.__modelInfo, function (n) {
           comp.setData({
-            checked: n===comp.data.value
+            checked: n === comp.data.value
           });
         });
 
@@ -390,18 +393,18 @@ function bindModelHandler(opt){
           checked: modelVal === comp.data.value
         });
       },
-      handler: function (params){
-       
-        if(!params.checked) return;
+      handler: function (params) {
+
+        if (!params.checked) return;
         var pageCache = util.app.globalData.__framework.pageCache, comp = this, modelInfo = comp.__modelInfo;
         var modelAttrName = modelInfo.attr;
         // var exp = comp.data[modelAttrName];
-        
+
         pageCache.updateModel(modelInfo, params.value);
       }
     },
     select: {
-      init: function(){
+      init: function () {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this;
 
         var modelVal = pageCache.registerModel(comp.__modelInfo, function (n) {
@@ -414,7 +417,7 @@ function bindModelHandler(opt){
           value: modelVal
         });
       },
-      handler: function(params){
+      handler: function (params) {
         var pageCache = util.app.globalData.__framework.pageCache, comp = this, modelInfo = comp.__modelInfo;
         var modelAttrName = modelInfo.attr;
         // var exp = comp.data[modelAttrName];
@@ -444,16 +447,16 @@ function bindModelHandler(opt){
     }
   };
 
-  for (var k in models){
+  for (var k in models) {
 
-    (function(type){
+    (function (type) {
       var modelUtil = models[type];
       var modelAttrName = 'v-model:' + type;
       properties[modelAttrName] = {
         type: String,
         observer: function (v, o) {
-          if(!v) return;
-          if (!this.__modelInfo){
+          if (!v) return;
+          if (!this.__modelInfo) {
             var comp = this;
             var modelInfo = {
               attr: modelAttrName,
@@ -475,47 +478,48 @@ function bindModelHandler(opt){
       }
 
     })(k);
-    
+
   }
-  
+
   // modelFrom属性指明该model的值来源，为来源组件的id，若不指定则来源于当前页
   properties.modelFrom = {
     type: String,
     observer: function (v, o) {
       var modelUtil = this.__modelUtil;
-      if (modelUtil){
+      if (modelUtil) {
         // 当绑定id变化时，重新注册
         modelUtil.init && modelUtil.init.call(this);
       }
     }
   };
-  
+
 }
 
 
 
-module.exports = function(tagName, absolutePath){
+module.exports = function (tagName, absolutePath) {
   // 某个地址转为相对的绝对地址
-  var getAbsolute = function(url){
-    if(url.indexOf('/')===0) return url;
+  var getAbsolute = function (url) {
+    if (url.indexOf('/') === 0) return url;
     var urls = url.split('/');
-    var pageUrl = absolutePath, pages = pageUrl.split('/');
+    var pageCache = util.app.globalData.__framework.pageCache;
+    var pageUrl = absolutePath || pageCache.getPagePath(), pages = pageUrl.split('/');
     pages.pop(); // 去掉同级
     var arr = [], cur;
-    while (cur = urls.shift()){
-    	if(cur==='.'){
+    while (cur = urls.shift()) {
+      if (cur === '.') {
 
-       	}else if(cur==='..'){
-        	pages.pop();
-        }else{
-          arr.push(cur);
-        }
+      } else if (cur === '..') {
+        pages.pop();
+      } else {
+        arr.push(cur);
+      }
     }
     var arr = pages.concat(arr);
     return arr.join('/');
   };
   var _Page = function (opt) {
-    addPageLifetimes(opt);
+    addPageLifetimes(opt, absolutePath);
     addMethods(opt, absolutePath, true);
     Page(opt);
   };
